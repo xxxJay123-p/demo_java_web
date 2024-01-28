d741a9f15215823c63ee7ac2fc7eaf2c79ed4354
xxxJay123 <jay6677884@gmail.com>
2024-01-28 16:04:26 +0800

    Update: Finished all

--- src/components/contact/Location.jsx
+++ src/components/contact/Location.jsx
@@ -13,14 +13,17 @@ function Location() {
       <div className="map__bg">
         <div className="map__container">
           <div className="map" data-aos="flip-left">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7382.833743826122!2d114.16961399357906!3d22.3000686!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x340400ef646de2a5%3A0x43755790396a351e!2sSCOPE%20TST%20East%20Learning%20Centre!5e0!3m2!1szh-TW!2shk!4v1702459094999!5m2!1szh-TW!2shk"
-              width="100%"
-              height="450"
-              allowFullScreen=""
+            {/* <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3690.4296740763248!2d114.16956262475043!3d22.33739929155078!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34040733e2820eb5%3A0xed1df9ccf98e6fe4!2z6aaZ5riv5Z-O5biC5aSn5a245bCI5qWt6YCy5L-u5a246Zmi!5e0!3m2!1szh-TW!2shk!4v1706428807616!5m2!1szh-TW!2shk"
+              width="800"
+              height="600"
+              style="border:0;"
+              allowfullscreen=""
               loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-            ></iframe>
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe> */}
+            <h1> Error to use iframe in React</h1>
+            <h1>Need to use Google Map API</h1>
           </div>
           <div className="addrees"></div>
         </div>
